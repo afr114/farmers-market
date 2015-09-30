0e668eb82605b0aa537a6558f7acc650c86fa3f8
lisa folliard & abdul rahimi <student@epicodus.com>
2015-09-30 12:08:45 -0700

    delete a vendor

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,6 +10,13 @@ export default Ember.Route.extend({
       var newVendor = this.store.createRecord('vendor', params);
       newVendor.save();
       this.transitionTo('index');
+    },
+
+    deleteVendor(vendor) {
+      if (confirm('Are you sure you want to delete this vendor')) {
+      vendor.destroyRecord();
+      this.transitionTo('index');
+      }
     }
   }
 });
